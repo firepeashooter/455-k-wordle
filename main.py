244da4f42c5a66a687cce794d6cc72cc0870af96
BrettTheHobbit <Bretthhobbs@gmail.com>
2026-03-17 22:03:15 -0400

    [IMPLEMENT] Tourney Selection, fitness also works with solutions of all sizes now

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import numpy as np
-import answer # generates 
+import answer # generates an answer
+import copy
 
 # --- CONSTANTS ---
 ANSWER = []
@@ -17,22 +18,62 @@ def evaluate_fitness(guess=[], real_answer=[]):
     fitness = 0
 
     # for each char in the guess
-    for c_index in range(len(guess)):
-        if c_index <= len(real_answer): # ensures index never hits out of bounds
+    for c_index, char in enumerate(guess):
+        if c_index < len(real_answer): # ensures index never hits out of bounds
             # if the location lines up 1:1 with the answer, add 1 to fitness
-            if guess[c_index] == real_answer[c_index]:
+            if char == real_answer[c_index]:
                 fitness += 1
             # else if the char is in the word but wrong spot, add 0.5 to fitness
-            elif guess[c_index] in real_answer: # BE ADVISED - YELLOWS MAY BE TAGGED INCORRECTLY DUE TO DUPLICATES !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+            elif char in real_answer: # BE ADVISED - YELLOWS MAY BE TAGGED INCORRECTLY DUE TO DUPLICATES !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
                 fitness += 0.5
             # else add 0 to fitnes
         else:
-            continue
+            if char in real_answer:
+                fitness += 0.5
 
     # normalize fitness result and return
     fitness = fitness/len(guess)
 
     return fitness
+
+def tourney_selection(k=5, pop=[], answer=[]):
+    """
+    pick individuals as parents for the next generation. 
+    For tournament, pick a small random subset (tournament) and the best one wins the right to reproduce
+
+    k - size of each tournament
+    pop - the full population we are selecting from
+    answer - verifies fitness
+
+    parent_list - a list of solutions that have clearence to be parents.
+    """
+    # initialize list of solutions with clearence to be parents
+    parent_list = []
+    pop_size = len(pop)
+    target_size = pop_size // 2
+    
+    # while the list of parents isn't large enough, we continue tournaments
+    while len(parent_list) < target_size:
+        # a single loop will run ONE tournament
+        # pick a subset from pop of size k
+        p_indices = np.random.choice(len(pop), size=k, replace=False)
+        t_participants = [pop[i] for i in p_indices]
+
+
+        # add the one with the highest fitness to the parent list
+        best_sol = None
+        best_fit = -1
+
+        for p in t_participants:
+            new_fit = evaluate_fitness(p, answer)
+
+            if new_fit > best_fit: # replace best recorded fitness
+                best_sol = p
+                best_fit = new_fit
+
+        parent_list.append(best_sol)
+
+    return parent_list
 
 def main():
     """executes a full EA"""
